ce786786d9970ddce95b0f608d4105fb6c819fe2
Toyo1007 <toyotake1007@gmail.com>
2025-01-10 16:10:48 +0900

    コミット

--- Build/MSPhoton.framework.js
+++ Build/MSPhoton.framework.js
@@ -2078,13 +2078,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  6149200: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 6149261: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 6149325: function() {return Module.webglContextAttributes.powerPreference;},  
- 6149383: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 6149438: function($0) {performance.now = function() { return $0; };},  
- 6149486: function($0) {performance.now = function() { return $0; };},  
- 6149534: function() {performance.now = Module['emscripten_get_now_backup'];}
+  6149232: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 6149293: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 6149357: function() {return Module.webglContextAttributes.powerPreference;},  
+ 6149415: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 6149470: function($0) {performance.now = function() { return $0; };},  
+ 6149518: function($0) {performance.now = function() { return $0; };},  
+ 6149566: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
